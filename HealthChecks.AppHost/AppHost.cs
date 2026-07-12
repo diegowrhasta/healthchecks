466ce401a5a08902b2c6ca55760198e9ca5c15f5
Diego Balderrama <diegowrhasta@gmail.com>
2026-07-12 11:58:04 +0900

    Completes prototype for healthcheck dashboard by leveraging prometheus native capabilities (new dashboard)

--- HealthChecks.AppHost/AppHost.cs
+++ HealthChecks.AppHost/AppHost.cs
@@ -1,12 +1,62 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var apiService = builder.AddProject<Projects.HealthChecks_ApiService>("apiservice")
-    .WithHttpHealthCheck("/health");
+// Prometheus
+var prometheus = builder.AddContainer("prometheus", "prom/prometheus:main-distroless")
+    .WithBindMount("./prometheus/prometheus.yml", "/etc/prometheus/prometheus.yml")
+    .WithHttpEndpoint(targetPort: 9090, port: 9090)
+    .WithLifetime(ContainerLifetime.Persistent);
 
-builder.AddProject<Projects.HealthChecks_Web>("webfrontend")
-    .WithExternalHttpEndpoints()
+// Grafana
+var grafana = builder.AddContainer("grafana", "grafana/grafana:nightly-ubuntu-slim")
+    .WithEnvironment("GF_SECURITY_ADMIN_USER", "admin")
+    .WithEnvironment("GF_SECURITY_ADMIN_PASSWORD", "admin")
+    .WithBindMount(
+        "./grafana/provisioning",
+        "/etc/grafana/provisioning")
+    .WithBindMount(
+        "./grafana/dashboards",
+        "/var/lib/grafana/dashboards")
+    .WithHttpEndpoint(targetPort: 3000, port: 3000)
+    .WithLifetime(ContainerLifetime.Persistent)
+    .WaitFor(prometheus);
+
+// var alloy = builder.AddContainer("alloy", "grafana/alloy:v1.17.1")
+//     .WithBindMount(
+//         "./alloy/config.alloy",
+//         "/etc/alloy/config.alloy")
+//     .WithHttpEndpoint(
+//         targetPort: 12345,
+//         port: 12345,
+//         name: "ui")
+//     .WithEndpoint(
+//         targetPort: 4317,
+//         port: 4317,
+//         name: "otlp-grpc")
+//     .WithEndpoint(
+//         targetPort: 4318,
+//         port: 4318,
+//         name: "otlp-http")
+//     .WithEndpoint(
+//         targetPort: 9464,
+//         port: 9464,
+//         name: "prometheus")
+//     .WithArgs(
+//         "run",
+//         "/etc/alloy/config.alloy")
+//     .WithLifetime(ContainerLifetime.Persistent)
+//     .WaitFor(prometheus);
+
+var apiService = builder
+    .AddDockerfile(name: "apiservice", contextPath: "../", dockerfilePath: "./HealthChecks.ApiService/Dockerfile")
+    .WithHttpEndpoint(targetPort: 8080, port: 5591)
+    .WithHttpHealthCheck("/health");
+    // .WithEnvironment("OTEL_EXPORTER_OTLP_ENDPOINT", "http://alloy:4317")
+    // .WithEnvironment("OTEL_EXPORTER_OTLP_PROTOCOL", "grpc");
+
+builder
+    .AddDockerfile(name: "webfrontend", contextPath: "../", dockerfilePath: "./HealthChecks.Web/Dockerfile")
+    .WithHttpEndpoint(targetPort: 8080, port: 5208)
     .WithHttpHealthCheck("/health")
-    .WithReference(apiService)
     .WaitFor(apiService);
 
 builder.Build().Run();
--- README.md
+++ README.md
@@ -3,3 +3,32 @@
 Project built with .NET Aspire to test out a prototype that leverages Prometheus and Grafana to build a neat HealthChecks 
 dashboard to which the health of different services can be looked at a quick glance.
 
+## Docker on Aspire
+
+Apparently, just so you can build with Aspire, you need a `docker-buildx` plugin installed, otherwise Aspire won't be able 
+to start the docker build process as he wants to.
+
+But this enables you to then always on aspire start, build images that you then spin up:
+
+````csharp
+var apiService = builder
+    .AddDockerfile(name: "apiservice", contextPath: "../", dockerfilePath: "./HealthChecks.ApiService/Dockerfile")
+    .WithHttpEndpoint(targetPort: 8080, port: 5591)
+    .WithHttpHealthCheck("/health");
+````
+
+Now, this comes with the caveat that it takes a long time to start. But for this specific prototype it makes sense because 
+we really want to emulate a whole architecture that's already interconnected, and to get across the hurdle of in-process apps 
+that can't be reliably connected from container's we can just make them containers themselves.
+
+## Healthcheck dashboard
+
+Found a better, sleek, and clean way of making the dashboard. (Better than healthchecks.io). It leverages the native functionalities 
+of Prometheus and then its connection to Grafana.
+
+Because we can define in prometheus different jobs to scrape information, when querying inside of it, we can use literal 
+syntax to get info on all these scraping jobs. And with that in mind, if a job goes down in scraping, we can arguably determine 
+that the service is down, or has issues, because a simple scrape of its metrics endpoint should be healthy and working if the 
+service is healthy and working.
+
+This even takes away the need of having to code ourselves some sorth of health, a literal scraping would be more than enough.
